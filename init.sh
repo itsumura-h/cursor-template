85c514d60f6799f732340b6c3b52bb7fbaa1e43d
itsumura-h <dumblepy@gmail.com>
2025-12-06 09:13:36 +0900

    .

--- init.sh
+++ init.sh
@@ -19,8 +19,8 @@ echo ".cursor/rules ディレクトリを作成しました: $RULES_DIR"
 # リポジトリ設定（common_rule_url などで使用するため先に定義）
 owner_repo="itsumura-h/cursor-template"
 
-# .cursor/common_rule.mdc を常にダウンロードして上書き
-COMMON_RULE_TARGET="$CURSOR_DIR/common_rule.mdc"
+# .cursor/rules/common_rule.mdc を常にダウンロードして上書き
+COMMON_RULE_TARGET="$RULES_DIR/common_rule.mdc"
 common_rule_url="https://raw.githubusercontent.com/$owner_repo/main/.cursor/common_rule.mdc"
 echo ".cursor/common_rule.mdc をダウンロードして上書きします: $COMMON_RULE_TARGET"
 success=false
--- readme.md
+++ readme.md
@@ -2,5 +2,5 @@ Cursor rules templates
 ===
 
 ```sh
-curl https://raw.githubusercontent.com/itsumura-h/cursor-template/refs/heads/main/init.sh -sSf | sh
+curl -H "Cache-Control: no-cache" -H "Pragma: no-cache" "https://raw.githubusercontent.com/itsumura-h/cursor-template/refs/heads/main/init.sh?$(date +%s)" -sSf | sh
 ```
